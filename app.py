f0e6850c209aa55c8c28fce9624c2b5967eb62bb
Santiago Lucini <santilucini@gmail.com>
2024-03-14 15:37:19 -0300

    add HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World, wuujuu"
+    #this executes the page
+    return render_template('home.html')
 
 
 #This is to excecute the app with Flask
