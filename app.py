5a6a51ba6e356bcffbd66b898b3f7304e815db04
Santiago Lucini <santilucini@gmail.com>
2024-03-14 16:57:01 -0300

    Added dynamic data and api

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+TRANSACTIONS = [
+    {
+        'id': 1,
+        'date': '01/01/1999',
+        'category': 'Supermarket',
+        'desc': 'Monster',
+        'income': '0',
+        'outcome': '1500'
+    },
+    {
+        'id': 2,
+        'date': '01/02/1999',
+        'category': 'Sueldo',
+        'desc': 'Work',
+        'income': '10000',
+        'outcome': '0'
+    }
+]
 
 @app.route("/")
 def hello_world():
     #this executes the page
-    return render_template('home.html')
+    return render_template('home.html',transactions=TRANSACTIONS)
 
+@app.route("/api/transactions")
+def list_transactions():
+    return jsonify(TRANSACTIONS)
 
 #This is to excecute the app with Flask
 if __name__ == "__main__":
