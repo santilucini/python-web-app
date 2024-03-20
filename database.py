236fa1154084bf32d1f38836f2530cc5446ed2a2
Santiago Lucini <santilucini@gmail.com>
2024-03-20 15:50:56 -0300

    Database update

--- database.py
+++ database.py
@@ -1,13 +1,19 @@
 from sqlalchemy import create_engine, text
+import os
 
-engine = create_engine("")
+with open('SECRET.txt','r') as file:
+    connection_string = file.read()
+
+engine = create_engine(str(connection_string))
 
 def load_transactions_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("select * from transactions;"))
-
+      
         transactions = []
         for row in result.all():
             transactions.append(dict(row._mapping))
 
         return transactions
+    
+print(load_transactions_from_db())
