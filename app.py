62a8b44f199fbe3a79567c43fc5a2dc27412fdea
Santiago Lucini <santilucini@gmail.com>
2024-08-31 19:03:18 -0300

    Savings

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-from database import load_transactions_from_db, load_transaction_from_db, add_new_transaction, get_initial_balance, get_categories,update_transaction
+from database import load_transactions_from_db, load_transaction_from_db, add_new_transaction, get_initial_balance, get_categories,update_transaction,load_from_db_by_category,get_last_inserted
+from database import add_new_transaction_saving
 from datetime import datetime
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -13,20 +17,22 @@ def index():
     try:
         transactions = load_transactions_from_db()
 
-        initial_balance = get_initial_balance()
+        initial_balance = get_initial_balance(1,"amount_arg")
+        
 
         balance = initial_balance
 
         for transaction in transactions:
             if transaction['type'] == 1:
-                balance -= transaction['amount']
+                balance -= transaction['amount_arg']
             elif transaction['type'] == 2:
-                balance += transaction['amount']
+                balance += transaction['amount_arg']
 
             transaction['balance'] = balance
 
         transactions.reverse()
         #this executes the page
+        #return jsonify(transactions)
         return render_template('base.html',page='expenses.html',transactions=transactions,balance=balance, initial_balance=initial_balance)
     except Exception as e:
         return e
@@ -34,7 +40,25 @@ def index():
 @app.route("/savings")
 def savings():
     try:
-        return render_template('base.html',page='savings.html')
+        transactions = load_from_db_by_category(3)
+
+        initial_balance = get_initial_balance(23,'amount_usd')
+
+        balance = initial_balance
+
+        for transaction in transactions:
+            if transaction['type'] == 1:
+                balance -= transaction['amount_usd']
+            elif transaction['type'] == 2:
+                balance += transaction['amount_usd']
+
+            transaction['balance'] = balance
+
+        transactions.reverse()
+        #this executes the page
+        #return jsonify(transactions)
+        #return render_template('base.html',page='expenses.html',transactions=transactions,balance=balance, initial_balance=initial_balance)
+        return render_template('base.html',page='table_template.html',transactions=transactions,balance=balance,initial_balance=initial_balance)
     except Exception as e:
         return e
 
@@ -42,6 +66,26 @@ def savings():
 def debts():
     try:
         return render_template('base.html',page='debts.html')
+    except Exception as e:
+        return e
+
+@app.route("/dashboard")
+def dashboard():
+    """
+    fig, ax = plt.subplots()
+    x = [0, 1, 2, 3, 4]
+    y = [0, 1, 4, 9, 16]
+    ax.plot(x, y)
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    #return render_template('index.html', plot_url=plot_url)
+    """
+    try:
+        return render_template('base.html',page='dashboard.html', 
+                               #plot_url=plot_url
+                               )
     except Exception as e:
         return e
 
@@ -63,7 +107,7 @@ def new_transaction():
     today = datetime.now().strftime("%Y-%m-%d")
 
     #this executes the page
-    return render_template('new_transaction.html',categories=categories,today=today)
+    return render_template('base.html',page='new_transaction.html',categories=categories,today=today)
 
 @app.route("/edit/<id>")
 def edit_transaction(id): 
@@ -80,9 +124,19 @@ def edit_transaction(id):
 def submit_new():
     # Handle POST request
     data = request.form
-
+    
     add_new_transaction(data)    
+    
+    
+    if data['Category'] == '3':
+        last_id = get_last_inserted()
+        savings_data = {
+            "id_transaction": last_id,
+            "amount": data['Amount_USD']
+        }       
 
+        add_new_transaction_saving(savings_data)
+    
     #this executes the page
     return redirect(url_for('index'))
     #return jsonify(data)
--- database.py
+++ database.py
@@ -1,14 +1,20 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, insert
 import os
 
-with open('SECRET.txt','r') as file:
+# Get the directory of the script
+script_dir = os.path.dirname(__file__)
+# Construct the full path to the file
+file_path = os.path.join(script_dir, 'SECRET.txt')
+
+with open(file_path,'r') as file:
     connection_string = file.read()
 
 engine = create_engine(str(connection_string)).execution_options(isolation_level="AUTOCOMMIT")
 
-def get_initial_balance():
+def get_initial_balance(id_category,field):
     with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM transactions WHERE category = '1';"))
+        query = "SELECT "+field+" as amount FROM transactions WHERE category = :id_category;"
+        result = conn.execute(text(query),{"field":field,"id_category":id_category})
       
         rows = result.all()        
         if len(rows) == 0:
@@ -16,11 +22,11 @@ def get_initial_balance():
         else:  
             for row in rows:
                 transaction = dict(row._mapping)
-            return transaction['amount']  
+            return transaction['amount']
 
 def load_transactions_from_db():
     with engine.connect() as conn:
-        result = conn.execute(text("SELECT id,date,c.Category as category,t.description as description,amount,type FROM transactions AS t LEFT JOIN categories AS c ON t.category=c.id_category WHERE c.id_category<>1"))
+        result = conn.execute(text("SELECT id,date,c.Category as category,t.description as description,amount_arg,amount_usd,type FROM transactions AS t LEFT JOIN categories AS c ON t.category=c.id_category WHERE c.id_category<>1"))
       
         transactions = []
         for row in result.all():
@@ -30,7 +36,7 @@ def load_transactions_from_db():
     
 def load_transaction_from_db(id):
     with engine.connect() as conn:
-        result = conn.execute(text("select id,c.id_category,c.Category as category,t.description,date,type,amount from transactions t inner join categories c ON t.category = c.id_category where id = :val"),
+        result = conn.execute(text("select id,c.id_category,c.Category as category,t.description,date,type,amount_arg,amount_usd from transactions t inner join categories c ON t.category = c.id_category where id = :val"),
                               {"val": id})
       
         rows = result.all()        
@@ -40,22 +46,41 @@ def load_transaction_from_db(id):
             for row in rows:
                 transaction = dict(row._mapping)
             return transaction   
+
+def load_from_db_by_category(id_category):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT id,date,c.Category as category,t.description as description,amount_arg,amount_usd,type FROM transactions AS t LEFT JOIN categories AS c ON t.category=c.id_category WHERE c.id_category = :id_category"),
+                              {"id_category":id_category})
+      
+        transactions = []
+        for row in result.all():
+            transactions.append(dict(row._mapping))
+
+        return transactions
         
 
 def add_new_transaction(data):
     with engine.connect() as conn:
-        query = text("INSERT INTO transactions (date,category,description,type,amount) VALUES (:date, :category, :description, :type, :amount)")        
+        query = text("INSERT INTO transactions (date,category,description,type,amount_arg,amount_usd) VALUES (:date, :category, :description, :type, :amount_arg, :amount_usd)")       
         
-        response = conn.execute(query,{"date":data['Date'],"category":data['Category'],"description":data['Description'],"type":data['Type'],"amount":data['Amount']})
+        response = conn.execute(query,{"date":data['Date'],"category":data['Category'],"description":data['Description'],"type":data['Type'],"amount_arg":data['Amount_ARG'],"amount_usd":data['Amount_USD']})
         #response = conn.execute(query,date=data['Date'],category=data['Category'],description=data['Description'],income=data['Income'],outcome=data['Outcome'])
-       
+        
+        return response
+
+def add_new_transaction_saving(data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO savings (id_transaction,amount) VALUES (:id_transaction, :amount)")       
+        
+        response = conn.execute(query,{"id_transaction":data['id_transaction'],"amount":data['amount']})        
+        
         return response
 
 def update_transaction(data):
     with engine.connect() as conn:
-        query = text("UPDATE [dbo].[transactions] SET [category] = :category,[description] = :description,[date] = :date,[type] = :type,[amount] = :amount WHERE [id] = :id")        
+        query = text("UPDATE [dbo].[transactions] SET [category] = :category,[description] = :description,[date] = :date,[type] = :type,[amount_arg] = :amount_arg, [amount_usd] = :amount_usd WHERE [id] = :id")        
         
-        response = conn.execute(query,{"date":data['Date'],"category":data['Category'],"description":data['Description'],"type":data['Type'],"amount":data['Amount'],"id":data['id']})        
+        response = conn.execute(query,{"date":data['Date'],"category":data['Category'],"description":data['Description'],"type":data['Type'],"amount_arg":data['Amount_ARG'],"amount_usd":data['Amount_USD'],"id":data['id']})        
        
         return response
 
@@ -68,3 +93,16 @@ def get_categories():
             categories.append(dict(row._mapping))
 
         return categories
+    
+
+def get_last_inserted():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT Max(id) as Last_ID FROM transactions"))   
+       
+        rows = result.all()        
+        if len(rows) == 0:
+            return None
+        else:  
+            for row in rows:
+                transaction = dict(row._mapping)
+            return transaction['Last_ID']
